61895cf3c30a9fc529e888c13a416b991878c87e
Henry Law <henrylaw@e-fever.org>
2024-04-26 01:06:06 +0800

    fix lint error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import image from '../assets/images/kv_v04a.png';
+
 export default function Home() {
   return (
     <section className="hero">
-      <div className="hero-body" style={{aspectRatio: 1}}>
-	<img src={image.src}/>
+      <div className="hero-body" style={{ aspectRatio: 1 }}>
+        <img src={image.src} alt="HKOSCon 2024" />
       </div>
     </section>
   );
