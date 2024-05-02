1056713639b10a99f64cc9bd4ea418a055de4932
Gret Horrow <spots-trots0j@icloud.com>
2024-05-03 01:06:56 +0800

    design_2024: missed a line

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import './page.scss';
 
 export default function Home() {
   return (
-    <main className="container">
+    <main>
       <div className="hero homePage__cover">&nbsp;</div>
     </main>
   );
