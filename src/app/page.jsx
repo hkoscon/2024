4e246a3eab92c81389760483a97fc1fa16bc688f
Gret Horrow <spots-trots0j@icloud.com>
2024-05-03 00:57:31 +0800

    design_2024: add responsive cover pic

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,7 @@ import './page.scss';
 export default function Home() {
   return (
     <main className="container">
-      <section className="hero homePage__cover">
-        <div className="hero-body homePage__cover__body">
-          <h1 className="title">HKOSCon 2024</h1>
-          <h2 className="subtitle has-text-weight-medium">Date: 5 - 6 Jul 2024</h2>
-          <h2 className="subtitle has-text-weight-medium">Venue: The University of Hong Kong (Tentative)</h2>
-        </div>
-      </section>
+      <div className="hero homePage__cover">&nbsp;</div>
     </main>
   );
 }
