cef88f6a7ac48b10c6609c8406e2cc4a2d36e7a5
Tony Yip <tony@tonyhhyip.me>
2024-05-04 01:30:59 +0800

    feat: optimize cover image size and serving
    Signed-off-by: Tony Yip <tony@tonyhhyip.me>

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,13 +2,11 @@ import './page.scss';
 
 export default function Home() {
   return (
-    <main>
-      <div className="hero homePage__cover">
-        <div className="is-hidden">
-          <h1>HKOSCon 2024</h1>
-          <h2>5 - 6 Jul, 2024</h2>
-          <h2>The University of Hong Kong (Tentative)</h2>
-        </div>
+    <main className="homePage__cover">
+      <div className="is-hidden">
+        <h1>HKOSCon 2024</h1>
+        <h2>5 - 6 Jul, 2024</h2>
+        <h2>The University of Hong Kong (Tentative)</h2>
       </div>
     </main>
   );
