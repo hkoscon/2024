14c7a5e8dc5a8483aed11ec9c5c2256a5208ef08
Tony Yip <tony@tonyhhyip.me>
2024-05-16 22:40:05 +0800

    feat: improve front page
    Signed-off-by: Tony Yip <tony@tonyhhyip.me>

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,21 @@
+import Image from 'next/image';
+import logo from '../components/navbar/brand.png';
 import './page.scss';
 
 export default function Home() {
   return (
-    <main className="homePage__cover">
-      <div className="is-hidden">
-        <h1>HKOSCon 2024</h1>
-        <h2>5 - 6 Jul, 2024</h2>
-        <h2>The University of Hong Kong (Tentative)</h2>
+    <main className="homePage">
+      <div className="homePage__cover">
+        <div className="container columns">
+          <div className="homePage__media column is-3-desktop is-flex">
+            <Image src={logo} alt="HKOSCon logo" className="homePage__logo m-auto" />
+          </div>
+          <div className="column is-align-self-center">
+            <h1 className="title has-text-brand-color-blue">HKOSCon 2024</h1>
+            <h2 className="subtitle">5 - 6 Jul, 2024</h2>
+            <h2 className="subtitle">The University of Hong Kong (Tentative)</h2>
+          </div>
+        </div>
       </div>
     </main>
   );
