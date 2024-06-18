0f77f4e83a1c2bc316c0108ddb822cdde6dd7dcd
Henry Law <henrylaw@e-fever.org>
2024-06-18 22:51:19 +0800

    fix supporting organization size and only show logo

--- src/app/sponsor/SponsorTier.jsx
+++ src/app/sponsor/SponsorTier.jsx
@@ -2,22 +2,54 @@ import PropTypes from 'prop-types';
 import SponsorInfo from './SponsorInfo';
 
 import './sponsorTier.scss';
+import './supportOrgInfo.scss';
 
 export default function SponsorTier({ name, sponsors }) {
+  if (name !== 'Supporting Organization') {
+    return (
+      <section className="section">
+        <h3 className="title sponsorTier__title">{name}</h3>
+        {sponsors.map(({
+          title, logo, description, links,
+        }) => (
+          <SponsorInfo
+            key={title}
+            title={title}
+            logo={logo}
+            description={description}
+            links={links}
+          />
+        ))}
+      </section>
+    );
+  }
+  /* Supporting Organization */
   return (
     <section className="section">
       <h3 className="title sponsorTier__title">{name}</h3>
-      {sponsors.map(({
-        title, logo, description, links,
-      }) => (
-        <SponsorInfo
-          key={title}
-          title={title}
-          logo={logo}
-          description={description}
-          links={links}
-        />
-      ))}
+      <div className="columns">
+        <div className="column">
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                <div className="columns sponsorInfo supportOrgInfo">
+                  {sponsors.map(({
+                    title, logo,
+                  }) => (
+                    <div className="column is-3" key={title}>
+                      <div className="card-image supportOrgAspect">
+                        <figure className="image sponsorInfo__logo">
+                          <img src={logo} alt={title} />
+                        </figure>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
