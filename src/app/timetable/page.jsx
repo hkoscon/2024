66208ae29fb7a27fa1b9fb56efeb5cb467832b4d
Henry Law <henrylaw@e-fever.org>
2024-06-26 23:19:44 +0800

    [workaround] separate data forum to its own timetable

--- src/app/timetable/page.jsx
+++ src/app/timetable/page.jsx
@@ -2,9 +2,50 @@ import { timetable } from '../data';
 import TimetableDay from './TimetableDay';
 
 export default function Page() {
+  // fixing Data Forum timetable
+  const newDay1 = {
+    date: timetable[0].date,
+    day: timetable[0].day,
+    timeslots: [],
+  };
+  const dataForumDay1 = {
+    date: timetable[0].date,
+    day: '1 (Data Forum)',
+    timeslots: [],
+  };
+  timetable[0].timeslots.forEach(({ startTime, endTime, events }) => {
+    const day1Events = [];
+    const dataForumEvents = [];
+    events.forEach((ev) => {
+      if (ev.venue.name === 'MWT2') {
+        dataForumEvents.push(ev);
+      } else {
+        day1Events.push(ev);
+      }
+    });
+    if (day1Events.length > 0) {
+      newDay1.timeslots.push({
+        startTime,
+        endTime,
+        events: day1Events,
+      });
+    }
+    if (dataForumEvents.length > 0) {
+      dataForumDay1.timeslots.push({
+        startTime,
+        endTime,
+        events: dataForumEvents,
+      });
+    }
+  });
+  const newTimeTable = [
+    newDay1,
+    dataForumDay1,
+    timetable[1],
+  ];
   return (
     <div className="container is-fluid">
-      {timetable.map(({ day, timeslots }) => (
+      {newTimeTable.map(({ day, timeslots }) => (
         <TimetableDay key={`day${day}`} day={day} timeslots={timeslots} />
       ))}
     </div>
